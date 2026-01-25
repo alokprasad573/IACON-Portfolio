6ce52e6a03f8390f49adfda6d4a1c2d092c10e1f
Alok Prasad <alokprasad573@outlook.com>
2026-01-26 00:22:55 +0530

    Initial Commit.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <main>
+            <h1>Alok Prasad</h1>
+            <p>Full Stack Developer</p>
+        </main>
+    );
+};
+
+export default App;
