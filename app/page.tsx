00e321b35ae03dbafaba6d566a8be975fceb68d5
Alok Prasad <alokprasad573@outlook.com>
2026-01-27 16:23:14 +0530

    Animation Added.

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,18 @@ export default function Home() {
 
   return (
     <div className="content-layer flex flex-col justify-center px-8 md:px-24 min-h-screen relative z-10">
-      <div className="max-w-7xl mx-auto w-full grid md:grid-cols-2 items-center gap-12">
 
+      {/*Top Content */}
+      <div className="fixed top-8 left-8 z-50 flex flex-row items-center gap-6 animate-y-axis">
+        <img
+          src="https://pngimg.com/uploads/transformers/transformers_PNG15.png"
+          alt="Bumblebee Logo"
+          className="w-15 h-15 cursor-pointer drop-shadow-[0_0_15px_rgba(255,215,0,0.5)] transition-transform duration-500 ease-[cubic-bezier(0.175,0.885,0.32,1.275)] hover:scale-110 hover:rotate-6"
+        />
+        <span className="font-orbitron test-xs text-zinc-400 tracking-[0.2em] text-md uppercase">TEAM : PRIME <br /> NICKNAME : BEAST HUNTERS<br />ROLE : WARRIOR</span>
+      </div>
+
+      <div className="max-w-7xl mx-auto w-full grid md:grid-cols-2 items-center gap-12">
         {/* Left Content */}
         <div className="space-y-8 order-2 md:order-1">
           <div className="space-y-4">
@@ -59,15 +69,6 @@ export default function Home() {
           <div className="absolute inset-0 bg-[#FFD700]/10 blur-[100px] rounded-full"></div>
           <HeroExperience />
         </div>
-      </div>
-      {/*Top Content */}
-      <div className="fixed top-8 left-8 z-50 flex flex-row items-center gap-6">
-        <img
-          src="https://pngimg.com/uploads/transformers/transformers_PNG15.png"
-          alt="Bumblebee Logo"
-          className="w-15 h-15 cursor-pointer drop-shadow-[0_0_15px_rgba(255,215,0,0.5)] transition-transform duration-500 ease-[cubic-bezier(0.175,0.885,0.32,1.275)] hover:scale-110 hover:rotate-6"
-        />
-        <span className="font-orbitron test-xs text-zinc-400 tracking-[0.2em] text-md uppercase">TEAM : PRIME <br /> NICKNAME : BEAST HUNTERS<br />ROLE : WARRIOR</span>
       </div>
     </div>
   );
